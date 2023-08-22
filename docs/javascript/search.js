1ba2b7d480bbf2a2889d3581b529cdce3cd9e15c
Aaron McGinn <aaron.mcginn@cerner.com>
2023-08-22 16:01:39 -0500

    user id method

--- docs/javascript/search.js
+++ docs/javascript/search.js
@@ -12,7 +12,7 @@ function initializeSearch(smartContext) {
     let tenant_key = smartContext.state.tokenResponse.tenant;
     let patient_id = smartContext.patient.id;
     let encntr_id = smartContext.encounter.id;
-    let provider_id = smartContext.user.id;
+    let provider_id = smartContext.user.id();
     let username = smartContext.state.tokenResponse.username;
 
     let params = []
